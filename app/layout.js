ba9a60dff14ede765a8b8d952731b6cf7c5d3cc9
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-03-07 17:49:32 -0500

    load font

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,14 @@
 import Navigation from "./_components/Navigation";
 import "./_styles/styles.css";
 
+
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 export const metadata = {
   title: {
     template: "%s | WEAR AFRICA",
@@ -12,7 +20,7 @@ export const metadata = {
 export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <body>
+      <body className={josefin.className}>
         <header>
           <Navigation />
         </header>
