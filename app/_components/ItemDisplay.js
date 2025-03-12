44b5f1ecac28e8a10dd71e52b444b5665d0a1350
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-03-11 20:41:56 -0400

    add transition effect to itemDisplay.js

--- app/_components/ItemDisplay.js
+++ app/_components/ItemDisplay.js
@@ -5,7 +5,7 @@ import "./itemDisplay.css";
 export default function ItemDisplay({src, href, name, height="350", width="300", alt=""}) { 
     return (
       <li className="item-list">
-        <Link href={href}>
+        <Link href={href} className="item-pop">
           <Image src={src} height={height} width={width} alt={alt } />
         </Link>
         <p className="what">{name}</p>
