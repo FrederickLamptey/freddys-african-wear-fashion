aa8f0ee372bdb831f032e38b5231b13bc4c11e52
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-03-12 18:04:43 -0400

    build cart page and css

--- app/account/cart/page.js
+++ app/account/cart/page.js
@@ -1,11 +1,22 @@
+import Link from "next/link";
+import "./cart.css";
+
 export const metadata = {
   title: 'Cart',
 };
 
 export default function Page() {
   return (
-    <div>
-      <h1>cart Page</h1>
-    </div>
+    <>
+      <div className="cart-page-container">
+        <h2 className="cart-heading">Your Purchases</h2>
+        <p className="cart-content-empty">
+          Your cart is empty. Check out{' '}
+          <Link href="/" style={{ color: '#1864ab' }}>
+            items
+          </Link>
+        </p>
+      </div>
+    </>
   );
 }
