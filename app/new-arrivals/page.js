8a6f0f4d2f60aea28a7cf593d7b30a89a22020f9
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-03-13 18:02:44 -0400

    build new arrivals page

--- app/new-arrivals/page.js
+++ app/new-arrivals/page.js
@@ -1,3 +1,29 @@
+import Image from "next/image";
+import ItemDisplay from "../_components/ItemDisplay";
+import "./new-arrivals.css";
+
 export default function Page() {
-    return <h1>New Arrivals</h1>
+
+    const newArrivals = [];
+    
+    return (
+      <>
+        <div className="new-arrivals-first-container">
+          <div className="new-arrivals-content-container">
+            <h1 className="new-arrivals-first-container-h">
+              NEW ARRIVALS FOR YOU 
+            </h1>
+            <p className="new-arrivals-first-container-p">
+              All departments new arrivals for you and loved ones.
+            </p>
+          </div>
+        </div>
+
+        <div className="new-arrivals-second-container">
+          <ul className="new-arrivals-second-container-ul">
+            <ItemDisplay src="/dinero-kaftan-2.jpg" href="#" height="400" width="352" name="Item 1" />
+          </ul>
+        </div>
+      </>
+    );
 }
