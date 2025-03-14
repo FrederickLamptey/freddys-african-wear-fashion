02c59be05f09d0fd62431fc760121939ff0c1f89
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-03-14 18:53:03 -0400

    build dynamic page and route

--- app/new-arrivals/[itemId]/page.js
+++ app/new-arrivals/[itemId]/page.js
@@ -1,35 +1,30 @@
-import BlackButton from '@/app/_components/blackButton';
+import BlackButton from '@/app/_components/BlackButton';
 import Image from 'next/image';
 import './itemid.css';
+import { getItem } from '@/app/_lib/data-service';
 
-const item = {
-  id: 70,
-  name: 'DINERO DEEP BLUE KAFTAN',
-  department: 'male',
-  regularPrice: 200,
-  discount: 0,
-  description: 'blue',
-  image: '/dinero-kaftan-6.jpg',
-};
+export default async function Page({ params }) {
+  const item = await getItem(params.itemId)
+  const { id, name, department, regularPrice, discount, description, image } =
+    item;
 
-export default function Page() {
   return (
     <>
       <div className="item-review-container">
         <ul className="item-review-ul">
           <li className="item-review-li">
-            <Image src={`${item.image}`} width="500" height="600" alt="" />
+            <Image src={`${image}`} width="500" height="600" alt="" />
           </li>
         </ul>
 
         <div className="item-card-details">
-          <h3 className="item-card-heading-1">{item.name}</h3>
-          <h4 className="item-card-heading-2">${item.regularPrice}</h4>
-          <p className="item-card-description">
-            DESCRIPTION: {item.description}
-          </p>
+          <h3 className="item-card-heading-1">{name}</h3>
+          <h4 className="item-card-heading-2">${regularPrice}</h4>
+          <p className="item-card-description">DESCRIPTION: {description}</p>
+          <p className="item-card-department">DEPARTMENT: {department}</p>
+          <p className="item-card-discount">DISCOUNT: ${discount}</p>
           <BlackButton href="#">ADD TO SHOPPING BAG</BlackButton>
-          <p className='item-card-model-description'>
+          <p className="item-card-model-description">
             MODEL SIZE: The model is 6ft tall and wears a large shirt and 36
             long pants.
           </p>
