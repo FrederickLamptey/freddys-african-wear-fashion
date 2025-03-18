17f8e3caba5899deea30bb148a87092d7a56fbd5
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-03-18 16:57:29 -0400

    modify itemid.css

--- app/new-arrivals/[itemId]/page.js
+++ app/new-arrivals/[itemId]/page.js
@@ -24,27 +24,27 @@ export default async function Page({ params }) {
           </li>
         </ul>
         <div className="item-card-details">
-            <h3 className="item-card-heading-1">{name}</h3>
-            <h4 className="item-card-heading-2">
-              <span className="item-card-span">ORIGINAL PRICE: </span>$
-              {regularPrice}
-            </h4>
-            <div className="item-card-des-dep-container">
-              <p className="item-card-description">
-                <span className="item-card-span">DESCRIPTION: </span>
-                {description}
-              </p>
-              <p className="item-card-department">
-                <span className="item-card-span">DEPARTMENT: </span>
-                {department}
-              </p>
-            </div>
-            <p className="item-card-discount">
-              <span className="item-card-span">DISCOUNT: </span>${discount}
+          <h3 className="item-card-heading-1">{name}</h3>
+          <h4 className="item-card-heading-2">
+            <span className="item-card-span">ORIGINAL PRICE: </span>$
+            {regularPrice}
+          </h4>
+          <div className="item-card-des-dep-container">
+            <p className="item-card-description">
+              <span className="item-card-span">DESCRIPTION: </span>
+              <span className="item-card-span-2">{description}</span>
             </p>
-            <p className="item-card-subtotal">
-              <span className="item-card-span">SUBTOTAL: </span>${subTotal}
+            <p className="item-card-department">
+              <span className="item-card-span">DEPARTMENT: </span>
+              <span className="item-card-span-2">{department}</span>
             </p>
+          </div>
+          <p className="item-card-discount">
+            <span className="item-card-span">DISCOUNT: </span>${discount}
+          </p>
+          <p className="item-card-subtotal">
+            <span className="item-card-span">SUBTOTAL: </span>${subTotal}
+          </p>
           <BlackButton href="#">ADD TO SHOPPING BAG</BlackButton>
           <p className="item-card-model-description">
             MODEL SIZE: The model is 6ft tall.
