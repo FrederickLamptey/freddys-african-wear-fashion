c0e97992702902383b1c1988e23bdb85babab3f2
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-03-19 16:35:03 -0400

    build male page

--- app/male/page.js
+++ app/male/page.js
@@ -1,11 +1,27 @@
+import ItemDisplay from "../_components/ItemDisplay";
+import { getItems } from "../_lib/data-service";
+import "./maleItems.css"
+
 export const metadata = {
   title: "Male wardrobe",
 }
 
-export default function Page() {
+export default async function Page() {
+  const items = await getItems()
+  const maleItems = items.filter(item => item.department === 'male');
+ 
   return (
-    <div>
-      <h1>Male Page</h1>
+    <div className="male-items-container">
+      <ul className="male-items-ul">
+        {maleItems.map((maleItem) => (
+          <ItemDisplay
+            src={maleItem.image}
+            href="#"
+            name={maleItem.name}
+            key={maleItem.id}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
