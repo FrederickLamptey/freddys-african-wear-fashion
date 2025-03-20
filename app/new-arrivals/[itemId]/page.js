45c8147a27ebf76ed5470dd5aee48cc038e23832
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-03-19 20:12:25 -0400

    edit itemId page and add more styles to itemid.css

--- app/new-arrivals/[itemId]/page.js
+++ app/new-arrivals/[itemId]/page.js
@@ -27,8 +27,8 @@ export default async function Page({ params }) {
         <div className="item-card-details">
           <h3 className="item-card-heading-1">{name}</h3>
           <h4 className="item-card-heading-2">
-            <span className="item-card-span">ORIGINAL PRICE: </span>$
-            {regularPrice}
+            <span className="item-card-span">ORIGINAL PRICE: </span>
+            <span className="item-card-span-h4">{`$${regularPrice}`}</span>
           </h4>
           <div className="item-card-des-dep-container">
             <p className="item-card-description">
@@ -41,10 +41,12 @@ export default async function Page({ params }) {
             </p>
           </div>
           <p className="item-card-discount">
-            <span className="item-card-span">DISCOUNT: </span>${discount}
+            <span className="item-card-span">DISCOUNT: </span>
+            {`$${discount}`}
           </p>
           <p className="item-card-subtotal">
-            <span className="item-card-span">SUBTOTAL: </span>${subTotal}
+            <span className="item-card-span">SUBTOTAL: </span>
+            {`$${subTotal}`}
           </p>
           <BlackButton>ADD TO SHOPPING BAG</BlackButton>
           {/* <p className="item-card-model-description">
