07a1f9f26046374377ac9bf92277007a41e3add3
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-03-27 21:03:02 -0400

    build and implement spinning in button

--- app/_components/DeleteCartItem.js
+++ app/_components/DeleteCartItem.js
@@ -2,14 +2,26 @@
 import { deleteCartItem } from '../_lib/actions';
 import { HiOutlineTrash } from 'react-icons/hi';
 import './deleteCartItem.css';
+import { useTransition } from 'react';
+import SpinnerMini from './SpinnerMini';
 
 function DeleteCartItem({ cartItemId }) {
+  const [isPending, startTransition] = useTransition();
+
+  function handleDelete() {
+    startTransition(() => deleteCartItem(cartItemId));
+  }
   return (
-    <button
-      className="cart-item-delete-btn"
-      onClick={() => deleteCartItem(cartItemId)}
-    >
-      <HiOutlineTrash /> <span>Remove</span>
+    <button className="cart-item-delete-btn" onClick={handleDelete}>
+      {!isPending ? (
+        <>
+          <HiOutlineTrash /> <span>Remove</span>{' '}
+        </>
+      ) : (
+        <span>
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   );
 }
