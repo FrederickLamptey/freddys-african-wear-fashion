ccba9e8a58ec9b50c1a9a1002285e1d219d43324
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-04-16 23:39:17 -0400

    restructure navigation

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -23,81 +23,140 @@ export default function Navigation({ firstName }) {
   };
 
   return (
+    // <nav>
+    //   <div className="nav-container">
+    //     <button className="menu-toggle" onClick={toggleMenu}>
+    //       {isOpen ? <FiX /> : <FiMenu />}
+    //     </button>
+
+    //     <ul className={`nav-list first-nav-list ${isOpen ? 'open' : ''}`}>
+    //       <li>
+    //         <Link href="/" style={{ textDecoration: 'none', color: 'black' }}>
+    //           <HiOutlineHome />
+    //         </Link>
+    //       </li>
+    //       <li>
+    //         <Link
+    //           href="/contact"
+    //           style={{ textDecoration: 'none', color: 'black' }}
+    //         >
+    //           <HiOutlinePhone />
+    //         </Link>
+    //       </li>
+    //       <li>
+    //         <Link
+    //           href="/cart"
+    //           style={{ textDecoration: 'none', color: 'black' }}
+    //         >
+    //           <HiOutlineShoppingBag />
+    //         </Link>
+    //       </li>
+    //       <li>
+    //         <Link
+    //           href="/account/profile"
+    //           style={{ textDecoration: 'none', color: 'black' }}
+    //         >
+    //           <HiOutlineUserCircle />
+    //         </Link>
+    //       </li>
+    //     </ul>
+
+    //     <h1 className="store-name">
+    //       <Link
+    //         href="/"
+    //         className="nav-storeName-link"
+    //         // style={{ textDecoration: 'none', color: 'black', display: 'block', width: 'auto' }}
+    //       >
+    //         WEAR AFRICA
+    //       </Link>
+    //     </h1>
+
+    //     <ul className={`nav-list second-nav-list ${isOpen ? 'open' : ''}`}>
+    //       {firstName ? (
+    //         <li className="welcome-guest">Welcome, {firstName}</li>
+    //       ) : (
+    //         ' '
+    //       )}
+    //       <li>
+    //         <Link
+    //           href="/about"
+    //           style={{ textDecoration: 'none', color: 'black' }}
+    //         >
+    //           About
+    //         </Link>
+    //       </li>
+    //       <li>
+    //         <Link
+    //           href="/male"
+    //           style={{ textDecoration: 'none', color: 'black' }}
+    //         >
+    //           Male
+    //         </Link>
+    //       </li>
+    //       <li>
+    //         <Link
+    //           href="/female"
+    //           style={{ textDecoration: 'none', color: 'black' }}
+    //         >
+    //           Female
+    //         </Link>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </nav>
     <nav>
       <div className="nav-container">
-        <button className="menu-toggle" onClick={toggleMenu}>
-          {isOpen ? <FiX /> : <FiMenu />}
-        </button>
-
-        <ul className={`nav-list first-nav-list ${isOpen ? 'open' : ''}`}>
-          <li>
-            <Link href="/" style={{ textDecoration: 'none', color: 'black' }}>
-              <HiOutlineHome />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/contact"
-              style={{ textDecoration: 'none', color: 'black' }}
-            >
-              <HiOutlinePhone />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/cart"
-              style={{ textDecoration: 'none', color: 'black' }}
-            >
-              <HiOutlineShoppingBag />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/account/profile"
-              style={{ textDecoration: 'none', color: 'black' }}
-            >
-              <HiOutlineUserCircle />
-            </Link>
-          </li>
-        </ul>
-
         <h1 className="store-name">
-          <Link
-            href="/"
-            className="nav-storeName-link"
-            // style={{ textDecoration: 'none', color: 'black', display: 'block', width: 'auto' }}
-          >
+          <Link href="/" className="nav-storeName-link">
             WEAR AFRICA
           </Link>
         </h1>
-
-        <ul className={`nav-list second-nav-list ${isOpen ? 'open' : ''}`}>
-          {firstName ? (
-            <li className="welcome-guest">Welcome, {firstName}</li>
-          ) : (
-            ' '
-          )}
+        
+        <button className="menu-toggle" onClick={toggleMenu}>
+          {isOpen ? <FiX /> : <FiMenu />}
+        </button>
+        <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
           <li>
-            <Link
-              href="/about"
-              style={{ textDecoration: 'none', color: 'black' }}
-            >
+            <Link href="/" className="navlink">
+              <div className="it-is-container">
+                <HiOutlineHome /> <span className="it-is">Home</span>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="navlink">
+              <div className="it-is-container">
+                <HiOutlinePhone /> <span className="it-is">Contact</span>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/cart" className="navlink">
+              <div className="it-is-container">
+                <HiOutlineShoppingBag /> <span className="it-is">Bag</span>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/account/profile" className="navlink">
+              <div className="it-is-container">
+                <HiOutlineUserCircle /> <span className="it-is">Profile</span>
+              </div>
+            </Link>
+          </li>
+          {firstName && <li className="welcome-guest">Welcome, {firstName}</li>}
+          <li>
+            <Link href="/about" className="navlink">
               About
             </Link>
           </li>
           <li>
-            <Link
-              href="/male"
-              style={{ textDecoration: 'none', color: 'black' }}
-            >
+            <Link href="/male" className="navlink">
               Male
             </Link>
           </li>
           <li>
-            <Link
-              href="/female"
-              style={{ textDecoration: 'none', color: 'black' }}
-            >
+            <Link href="/female" className="navlink">
               Female
             </Link>
           </li>
